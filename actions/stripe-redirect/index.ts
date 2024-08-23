62d90d5f12645325fdb44235d27bc59b15516059
Rakesh Choudhary <hello.monkeguy@gmail.com>
2024-08-23 23:17:31 +0530

    random

--- actions/stripe-redirect/index.ts
+++ actions/stripe-redirect/index.ts
@@ -51,7 +51,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
             price_data: {
               currency: "USD",
               product_data: {
-                name: "Taskify Pro",
+                name: "flowDeck Pro",
                 description: "Unlimited boards for your organization.",
               },
               unit_amount: 2000,
--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "Taskify",
+  name: "flowDeck",
   description: "Collaborate, manage projects and reach new productivity peaks.",
   keywords: [
     "reactjs",
